f23e7b570451457b7ccab0c70cf130ebf4d4a8ba
DillonHutch <dhutchin@uccs.edu>
2025-04-21 14:28:05 -0600

    messed with some debugging

--- Spires Of Trials/Assets/Scripts/Managers/GameDebug.cs
+++ Spires Of Trials/Assets/Scripts/Managers/GameDebug.cs
@@ -21,7 +21,7 @@ public class GameDebug : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.P))
         {
             EventManager.Instance.TriggerEvent("LoadNextLevel", "Sanctum");
-            RoundManager.ROUND_NUMBER = 74;
+            RoundManager.ROUND_NUMBER = 70;
         }
         if (Input.GetKeyDown(KeyCode.I))
         {
